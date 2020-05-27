8020f13d4e88332a0f8a08b575d047b7763744ef
Nitzan Jaitman <nitzan@cloudinary.com>
2020-05-27 08:33:58 +0300

    Add extensions-mechanism and singleton to `Cloudinary`

--- url-gen/src/main/kotlin/com/cloudinary/Cloudinary.kt
+++ url-gen/src/main/kotlin/com/cloudinary/Cloudinary.kt
@@ -3,7 +3,14 @@ package com.cloudinary
 import com.cloudinary.config.Configuration
 import com.cloudinary.util.cloudinaryUrlFromEnv
 
+var instance: Cloudinary? = null
+
+private val initLockObject = Any()
+
 class Cloudinary(val config: Configuration) {
+    private val extensionsLock = Any()
+    private val extensions = mutableMapOf<String, Any>()
+
     constructor(cloudinaryUrl: String) : this(Configuration.fromUri(cloudinaryUrl))
     constructor() : this(
         cloudinaryUrlFromEnv() ?: throw
@@ -17,5 +24,49 @@ class Cloudinary(val config: Configuration) {
         val builder = Url.Builder(config)
         url?.let { builder.it() }
         return builder.build()
+    }
+
+    /**
+     *  ***INTERNAL USE ONLY*** get a custom extensions (property) from this cloudinary instance. A default
+     *  must be provided - this method always returns a value.
+     */
+    fun getExtension(name: String, default: Any): Any {
+        return extensions[name] ?: synchronized(extensionsLock) {
+            setExtension(name, default)
+            default
+        }
+    }
+
+    /**
+     *  ***INTERNAL USE ONLY*** Set a custom extensions (property) on this cloudinary instance.
+     */
+    fun setExtension(name: String, extension: Any) {
+        synchronized(extensionsLock) {
+            check(!extensions.containsKey(name)) { "An extension can only be set once" }
+            extensions[name] = extension
+        }
+    }
+
+    companion object {
+        /**
+         * Init the singleton instance.
+         */
+        fun init(newInstance: Cloudinary = Cloudinary()) {
+            synchronized(initLockObject) {
+                if (instance == null) throw Error(IllegalStateException("Cloudinary init() must be called only once"))
+                instance = newInstance
+            }
+        }
+
+        /**
+         * Get the singleton instance. Note: an explicit call to init() is required before using get().
+         */
+        fun get(): Cloudinary {
+            check(instance != null) { "Cloudinary init() must be called before using singleton instance" }
+
+            // Though this is a var, it is private and cannot be modified from not-null to null (settable only once)
+            // so this is a safe unwrapping.
+            return instance!!
+        }
     }
 }
