2065a8a6d1dcb683443549f310aa81ba0902729f
adimiz1 <adi.mizrahi@cloudinary.com>
2022-03-28 17:20:24 +0300

    Add test for round corners as expression

--- url-gen/src/test/kotlin/com/cloudinary/transformation/CornerRadiusTest.kt
+++ url-gen/src/test/kotlin/com/cloudinary/transformation/CornerRadiusTest.kt
@@ -1,6 +1,7 @@
 package com.cloudinary.transformation
 
 import com.cloudinary.cldAssert
+import com.cloudinary.transformation.expression.Expression
 import org.junit.Test
 
 class CornerRadiusTest {
@@ -12,5 +13,6 @@ class CornerRadiusTest {
         cldAssert("r_20:30:40", Transformation().roundCorners(20, 30, 40))
         cldAssert("r_20:30:40:50", Transformation().roundCorners(20, 30, 40, 50))
         cldAssert("r_max", Transformation().roundCorners(RoundCorners.max()))
+        cldAssert("r_w_mul_2", Transformation().roundCorners(RoundCorners.Companion.byRadius(Expression("width * 2"))))
     }
 }
