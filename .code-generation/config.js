b895cc1abfeb5a7e999677bddd50af0301cb7a50
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2022-05-15 08:10:43 +0300

    Add support for `transcode`

--- .code-generation/config.js
+++ .code-generation/config.js
@@ -13,7 +13,7 @@ module.exports = {
     openActionChar: '(',
     closeActionChar: ')',
     unsupportedTxParams: ['fl_waveform', 'e_anti_removal:', 'fl_animated', 'l_fetch', 'l_text', 'u_text', 'af_', 'if_', 'e_fade', 'palette_f00', 'g_auto:ocr_text', '$overlaywidth_$mainvideowidth_div_3'],
-    unsupportedSyntaxList: ['stroke(', 'textFit(', 'Animated.edit', 'RoundCorners(', 'getVideoFrame', 'transcode('],
+    unsupportedSyntaxList: ['stroke(', 'textFit(', 'Animated.edit', 'RoundCorners(', 'getVideoFrame'],
     mainTransformationString: {
       openSyntaxString: {
         image: 'cloudinary.image {\n\tpublicId("#publicID")',
