b7ad53110e0b0ae93778b074a67d62351181424b
adimiz1 <adi.mizrahi@cloudinary.com>
2022-05-15 10:02:04 +0300

    Update transformation builder dependency

--- url-gen/build.gradle.kts
+++ url-gen/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    implementation(files("libs/transformation-builder-1.0.0.jar"))
+    implementation("com.cloudinary:kotlin-transformation-builder-sdk:1.0.0")
     implementation(kotlin("stdlib-jdk8"))
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
