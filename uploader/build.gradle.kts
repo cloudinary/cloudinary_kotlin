7f5fb02174a5df31b644e33ed5ee6447a1811d60
adimiz1 <adi.mizrahi@cloudinary.com>
2022-05-17 12:38:11 +0300

    Update transformation builder to 1.1.0

--- uploader/build.gradle.kts
+++ uploader/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    implementation(files("libs/transformation-builder-1.0.0.jar"))
+    implementation("com.cloudinary:kotlin-transformation-builder-sdk:1.1.0")
     api(project(":url-gen"))
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.9.2")
 
