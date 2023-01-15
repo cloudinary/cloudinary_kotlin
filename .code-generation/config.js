de00912316d8a10a85feeb4ce3723f8d3dcd3a08
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2023-01-15 10:02:06 +0200

    Update `config.js`

--- .code-generation/config.js
+++ .code-generation/config.js
@@ -12,8 +12,8 @@ module.exports = {
     closeQualifiersChar : ' }',
     openActionChar: '(',
     closeActionChar: ')',
-    unsupportedTxParams: ['fl_waveform', 'e_anti_removal:', 'fl_animated', 'l_fetch', 'l_text', 'u_text', 'af_', 'if_', 'e_fade', '$overlaywidth_$mainvideowidth_div_3'],
-    unsupportedSyntaxList: ['stroke(', 'textFit(', 'Animated.edit', 'RoundCorners(', 'getVideoFrame'],
+    unsupportedTxParams: ['if_', 'palette_f00', '$overlaywidth_$mainvideowidth_div_3'],
+    unsupportedSyntaxList: ['getVideoFrame'],
     mainTransformationString: {
       openSyntaxString: {
         image: 'cloudinary.image {\n\tpublicId("#publicID")',
