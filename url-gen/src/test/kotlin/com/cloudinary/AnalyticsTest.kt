60d35082dbb7e5aaae3238a88a7e7828e25022db
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2024-01-15 10:53:31 +0200

    Update analytics token

--- url-gen/src/test/kotlin/com/cloudinary/AnalyticsTest.kt
+++ url-gen/src/test/kotlin/com/cloudinary/AnalyticsTest.kt
@@ -7,10 +7,13 @@ class AnalyticsTest {
     @Test
     fun testAnalyticsStringGeneration() {
         var s = generateAnalyticsSignature("1.24.0", KotlinVersion(12, 0))
-        assertEquals("CAHAlhAMZAA0", s)
+        assertEquals("DAHAlhAMZAA0", s)
 
         s = generateAnalyticsSignature("1.24.0-beta.6", KotlinVersion(12, 0))
-        assertEquals("CAHAlhAMZAA0", s)
+        assertEquals("DAHAlhAMZAA0", s)
+
+        s = generateAnalyticsSignature("1.24.0-beta.6", KotlinVersion(12, 0), "A", "android13")
+        assertEquals("DAHAlhAMANA0", s)
 
     }
 }
